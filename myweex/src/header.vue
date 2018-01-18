3d1f122b6e0e73ea7323a533ec27dd766c36ec36
honghaowu <honghaowu@163.com>
2018-01-18 23:04:46 +0800

    weex笔记

---
+++ myweex/src/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="head">
+      <text class="t">你好，weex</text>
+  </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style scoped>
+    .head{}
+    .head .t{
+        color:blue;
+        text-align: center;
+        font-weight: bold;
+    }
+</style>
+
--- myweex/src/index.vue
+++ myweex/src/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrapper" @click="update">
+    <whhead></whhead>
     <image :src="logoUrl" class="logo"></image>
     <text class="title">Hello {{target}}</text>
     <text class="desc">Now, let's use vue to build your weex app.</text>
@@ -7,17 +8,18 @@
 </template>
 
 <style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
-  .logo { width: 360px; height: 156px; }
-  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+
 </style>
 
 <script>
+ import whhead from './header.vue';
   export default {
     data: {
       logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
       target: 'World'
+    },
+    components:{
+      whhead
     },
     methods: {
       update: function (e) {
